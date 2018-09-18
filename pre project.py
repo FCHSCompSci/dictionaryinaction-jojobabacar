5122ee2fa5f81c9f80515f9860625f875d79cdaa
Ba <babacar.ba@stu.jefferson.kyschools.us>
2018-09-18 08:57:34 -0400

    added this feature

---
+++ pre project.py
@@ -0,0 +1,14 @@
+def player_info:
+    dbfz={
+        'display_name':'zawarudo'
+        'ranked_matches':41,
+        'party_matches': 23,
+        'rank': 'saiyan',
+        'clan': 'fuzed_zamasu',
+        'bp': 130000,
+        'total_matches': 164,
+        'no_of_wins':64
+        'followers':0
+        
+        
+        
