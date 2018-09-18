1b1eb29922763593024f50e83672e0536e5e13b9
Ba <babacar.ba@stu.jefferson.kyschools.us>
2018-09-18 09:38:46 -0400

    finished dictionary

---
+++ pre project(2).py
@@ -0,0 +1,17 @@
+dbfz={
+    'display_name': 'zawarudo',
+    'total_matches':164,
+    'ranked_match_wins': 32,
+    'party_match_wins': 14,
+    'clan': 'fuzed_zamasu',
+    'bp':130000,
+    'header': 'fuzed_zamasu',
+    'followers':0,
+    'ring_match_wins':57,
+    'team': 'hit_bluevegeta_rosegokublack',
+    }
+
+        
+def play_dbfz_rank_match():
+
+#im kind of confused what to do from here
